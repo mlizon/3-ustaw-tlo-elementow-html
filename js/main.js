aaea1622b61bf6ab3c304bec3ac77da6f16205ec
mlizon <122919166+mlizon@users.noreply.github.com>
2023-03-05 16:41:47 +0100

    Warsztat DOM

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log('Warsztat - ustaw tło');
+
+const setBackground = () => {
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+
+    p1.classList.add('bg-red');
+    p2.classList.add('bg-yellow');
+
+}
+
+let btnSetBackground = document.getElementById('set-background');
+
+btnSetBackground.addEventListener('click', setBackground);
